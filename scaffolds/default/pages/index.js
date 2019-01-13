f3d06887a5522a45aeea3bbd56747703b7a296b0
Bunlong <bunlong.van@gmail.com>
2019-01-13 13:47:30 +0700

    Refactor :rocket:

--- scaffolds/default/pages/index.js
+++ scaffolds/default/pages/index.js
@@ -6,7 +6,6 @@ export default () => (
   <div>
     <Head title="Home" />
     <Nav />
-
     <div className="hero">
       <h1 className="title">Welcome to create-next-app-cli (Create Next.js App building tools)</h1>
       <p className="description">To get started, edit <code>pages/index.js</code> and save to reload.</p>
--- scaffolds/material/components/head.js
+++ scaffolds/material/components/head.js
@@ -2,6 +2,7 @@ import NextHead from 'next/head';
 import { string } from 'prop-types';
 
 const defaultDescription = '';
+const defaultKeywords = '';
 const defaultOGURL = '';
 const defaultOGImage = '';
 
@@ -9,8 +10,9 @@ const Head = (props) => (
   <NextHead>
     <meta charSet="UTF-8" />
     <title>{props.title || ''}</title>
-    <meta name="description" content={props.description || defaultDescription} />
     <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <meta name="description" content={props.description || defaultDescription} />
+    <meta name="keywords" content={props.keywords || defaultKeywords} />
     <link rel="icon" type="image/png" sizes="16x16" href="/static/favicon-16x16.png"/>
     <link rel="icon" type="image/png" sizes="32x32" href="/static/favicon-32x32.png" />
     <link rel="shortcut icon" href="/static/favicon.ico" />
@@ -31,6 +33,7 @@ const Head = (props) => (
 Head.propTypes = {
   title: string,
   description: string,
+  keywords: string,
   url: string,
   ogImage: string
 };
