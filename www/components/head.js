542eb07c4a3826ac9f14a0b62ca7ee69c60afa6f
Bunlong <bunlong.van@gmail.com>
2020-04-28 21:47:44 +0700

    Add facebook and twitter thumnail.

--- www/components/head.js
+++ www/components/head.js
@@ -20,15 +20,19 @@ const Head = (props) => (
     <link rel="shortcut icon" href="/static/favicon.ico" />
     <link rel="apple-touch-icon" sizes="180x180" href="/static/apple-touch-icon.png" />
     <link rel="mask-icon" href="/static/favicon-mask.svg" color="#000000" />
-    <meta property="og:url" content={props.url || defaultOGURL} />
-    <meta property="og:title" content={props.title || ''} />
-    <meta property="og:description" content={props.description || defaultDescription} />
-    <meta name="twitter:site" content={props.url || defaultOGURL} />
-    <meta name="twitter:card" content="summary_large_image" />
-    <meta name="twitter:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image" content={props.ogImage || defaultOGImage} />
-    <meta property="og:image:width" content="1200" />
-    <meta property="og:image:height" content="630" />
+    
+    <meta property='og:url' content='https://create-next-app.js.org' />
+    <meta property='og:type' content='website' />
+    <meta property='og:title' content='create-next-app' />
+    <meta property='og:description' content='Create Next App allows you to create a Next.js app by running one command.' />
+    <meta property='og:image' content='/static/create-next-app.png' />
+
+    <meta name='twitter:card' content='summary_large_image' />
+    <meta name='twitter:site' content='@bunlongvan' />
+    <meta name='twitter:creator' content='@bunlongvan' />
+    <meta name='twitter:title' content='create-next-app' />
+    <meta name='twitter:description' content='Create Next App allows you to create a Next.js app by running one command.' />
+    <meta name='twitter:image' content='https://create-next-app.js.org/static/create-next-app.png' />
   </NextHead>
 )
 
